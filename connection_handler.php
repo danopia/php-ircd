c321ef5299b255813096a9da86036215277d3aa9
Easton Elliott <easton@geekness.eu>
2014-03-29 04:19:12 -0600

    Replace hardcoded server version with version from . Remove declaration of  (no point in declaring it if we're going to get the real IP right after)

--- connection_handler.php
+++ connection_handler.php
@@ -33,7 +33,6 @@ if ($x = @socket_accept($sock)) { // Is there a new socket?
     //else
     //{ 
     send(array('nick' => 'new user', 'sock' => $x), ':' . $config['name'] . ' NOTICE AUTH :*** Looking up your hostname...');
-    $ip = '0.0.0.0';
     socket_getpeername($x, $ip); // Get their IP
     $host = dns_timeout($ip); // Reverse DNS
     socket_set_nonblock($x); // Nonblocking FTW
@@ -125,9 +124,9 @@ foreach ($conn as &$me) { // Loop through connections
                     $me['ident'] = $newident;
                     $me['realname'] = $newrealname;
                     send($me, ':' . $config['name'] . ' 001 ' . $me['nick'] . ' :Welcome to the ' . $config['net'] . ' IRC Network ' . $me['nick'] . '!' . $me['ident'] . '@' . $me['host']);
-                    send($me, ':' . $config['name'] . ' 002 ' . $me['nick'] . ' :Your host is ' . $config['name'] . ', running version Danoserv0.1.0');
+                    send($me, ':' . $config['name'] . ' 002 ' . $me['nick'] . ' :Your host is ' . $config['name'] . ', running version Danoserv '.$config['version']);
                     send($me, ':' . $config['name'] . ' 003 ' . $me['nick'] . ' :This server was created Fri Jul 13 19:22:25 2007');
-                    send($me, ':' . $config['name'] . ' 004 ' . $me['nick'] . ' ' . $config['name'] . ' Danoserv0.1.0 iowghraAsORTVSxNCWqBzvdHtGp lvhopsmntikrRcaqOALQbSeIKVfMCuzNTGj');
+                    send($me, ':' . $config['name'] . ' 004 ' . $me['nick'] . ' ' . $config['name'] . ' Danoserv '.$config['version'].' iowghraAsORTVSxNCWqBzvdHtGp lvhopsmntikrRcaqOALQbSeIKVfMCuzNTGj');
                     send($me, ':' . $config['name'] . ' 005 ' . $newnick . ' CMDS=KNOCK,MAP,DCCALLOW,USERIP NAMESX SAFELIST HCN MAXCHANNELS=10 CHANLIMIT=#:10 MAXLIST=b:60,e:60,I:60 NICKLEN=30 CHANNELLEN=32 TOPICLEN=307 KICKLEN=307 AWAYLEN=307 MAXTARGETS=20 :are supported by this server');
                     send($me, ':' . $config['name'] . ' 005 ' . $newnick . ' WALLCHOPS WATCH=128 SILENCE=15 MODES=12 CHANTYPES=# PREFIX=(qaohv)~&@%+ CHANMODES=beI,kfL,lj,psmntirRcOAQKVCuzNSMTG NETWORK=' . $config['net'] . ' CASEMAPPING=ascii EXTBAN=~,cqnr ELIST=MNUCT STATUSMSG=~&@%+ EXCEPTS :are supported by this server');
                     send($me, ':' . $config['name'] . ' 005 ' . $me['nick'] . ' INVEX :are supported by this server');
@@ -161,7 +160,7 @@ foreach ($conn as &$me) { // Loop through connections
                 break;
 
             case 'version':
-                send($me, ':' . $config['name'] . ' 005 ' . $me['nick'] . ' Danoserv0.1.0 ' . $config['name'] . ' FinWXOoZE3 [Windows Server 22003 Service Pack 2=2309');
+                send($me, ':' . $config['name'] . ' 005 ' . $me['nick'] . ' Danoserv '.$config['version'].' ' . $config['name'] . ' FinWXOoZE3 [Windows Server 22003 Service Pack 2=2309');
                 send($me, ':' . $config['name'] . ' 005 ' . $me['nick'] . ' CMDS=KNOCK,MAP,DCCALLOW,USERIP NAMESX SAFELIST HCN MAXCHANNELS=10 CHANLIMIT=#:10 MAXLIST=b:60,e:60,I:60 NICKLEN=30 CHANNELLEN=32 TOPICLEN=307 KICKLEN=307 AWAYLEN=307 MAXTARGETS=20 :are supported by this server');
                 send($me, ':' . $config['name'] . ' 005 ' . $me['nick'] . ' WALLCHOPS WATCH=128 SILENCE=15 MODES=12 CHANTYPES=# PREFIX=(qaohv)~&@%+ CHANMODES=beI,kfL,lj,psmntirRcOAQKVCuzNSMTG NETWORK=' . $config['net'] . ' CASEMAPPING=ascii EXTBAN=~,cqnr ELIST=MNUCT STATUSMSG=~&@%+ EXCEPTS :are supported by this server');
                 send($me, ':' . $config['name'] . ' 005 ' . $me['nick'] . ' INVEX :are supported by this server');
@@ -337,8 +336,9 @@ foreach ($conn as &$me) { // Loop through connections
 
                     if (isset($channels[strtolower($target)])) { // Does the channel exist yet?
                         $channel = $channels[strtolower($target)];
-                        if (in_array($me, $channel['nicks']))
+                        if (in_array($me, $channel['nicks'])){
                             break; // Is user already in the channel?
+                        }    
 
                         $names = $me['nick'];
                         foreach ($channel['nicks'] as $user) { // Inform everyone and also build up /names
